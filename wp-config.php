e0b0c54cb25cb300399a597edd30bf7311626b01
oleh1 <lyboleg@gmail.com>
2017-04-22 16:05:28 +0300

    2

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'sumdu');
+define('DB_NAME', 'sumdu2');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'l4b01991');
+define('DB_PASSWORD', '1');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
